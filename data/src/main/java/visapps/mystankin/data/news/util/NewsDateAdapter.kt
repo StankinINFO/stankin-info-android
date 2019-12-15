c1250f2473b64ea6d2949091605490e9226a4991
Viska97 <27818051+Viska97@users.noreply.github.com>
2019-12-15 18:18:52 +0300

    fix design and news date parsing

--- data/src/main/java/visapps/mystankin/data/news/util/NewsDateAdapter.kt
+++ data/src/main/java/visapps/mystankin/data/news/util/NewsDateAdapter.kt
@@ -1,29 +1,18 @@
 package visapps.mystankin.data.news.util
 
-import com.google.gson.JsonParseException
 import com.google.gson.TypeAdapter
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
-import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
 class NewsDateAdapter: TypeAdapter<Date>() {
 
-    private val datePatterns = arrayOf("yyyy-MM-dd HH:mm:ssZZZZZ", "yyyy-MM-dd HH:mm:ss.SSSSSSZZZZZ")
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
 
     override fun write(out: JsonWriter, value: Date) {
         throw NotImplementedError()
     }
 
-    override fun read(`in`: JsonReader): Date {
-        val dateString = `in`.nextString()
-        for(pattern in datePatterns) {
-            try{
-                return SimpleDateFormat(pattern, Locale.ENGLISH).parse(dateString) ?: Date()
-            }
-            catch (e: ParseException){ }
-        }
-        throw JsonParseException("Unable to parse date: $dateString");
-    }
+    override fun read(`in`: JsonReader): Date = dateFormat.parse(`in`.nextString()) ?: Date()
 }
